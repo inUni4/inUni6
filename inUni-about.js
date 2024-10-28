02d9cda93b2556b5fdc60a736eb0134eec998087
inUni4 <166997704+inUni4@users.noreply.github.com>
2024-10-29 01:36:49 +0200

    Update inUni-about.js

--- inUni-about.js
+++ inUni-about.js
@@ -5,14 +5,14 @@ const forthBtn = document.querySelector('#forthButton');
 const infoText = document.querySelector('.info');
 
 firstBtn.addEventListener('click', () => {
-  infoText.textContent = "This website was created to help students around the world juggle life, school and home chores without the stress of university applications.";
+  infoText.textContent = "This website was created to provide endless opportunities to the youth of South Africa.";
 });
 secondBtn.addEventListener('click', () => {
-  infoText.textContent = "With the course calculator we are gla to say thar we reduce the amount of time spent on university applications by 90%, whereas you do not even have to do much but matriculate with the most excellent results and make your parents proud with no distraction.";
+  infoText.textContent = "With the course calculator we are glad to say that you can check which courses you qualify for in multiple universities.";
 });
 thirdBtn.addEventListener('click', () => {
-  infoText.textContent = "There's also a chance for the user to win 1GB of data for free once they register.";
+  infoText.textContent = "Our goal is to empower and improve the lives of the South African youth, by giving them access to multiple opportunities";
 });
 forthBtn.addEventListener('click', () => {
-  infoText.textContent = "If you observe any errors within the website be sure to contact us as soon as possible..";
+  infoText.textContent = "If you observe any errors within the website be sure to contact us as soon as possible.";
 });
